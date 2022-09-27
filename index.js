31f59365d7c2d0a11ed1c22f1e424f2c0ff5cb20
jyyoo <jyyoo@autocrypt.io>
2022-09-27 23:13:13 +0900

    지난 콜 목록 삭제, async await 추가

--- index.js
+++ index.js
@@ -1,32 +1,51 @@
 const {mysql} = require('./datasource/rds')
 const{frdb} = require('./datasource/firebase')
-const {ref, onValue, update} = require('firebase/database')
+const {ref, onValue, update, remove} = require('firebase/database')
 
-try {
-    mysql.query(
-        sql = 'select * from driver where is_deleted=false',
-        callback = async (err, rows) => {
-        if (err) {
-            console.log({err})
-        } else {
-            console.log('result of rows = ', rows.length)
-            for (const row of rows) {
-                let driverRef = ref(frdb, '/driverList/' + row.id)
-                onValue(driverRef, async (snapshot) => {
-                    const data = snapshot.val()
-                    if (data != null && data.callReceivingStatus != null && (row.call_receiving_status != data.callReceivingStatus)) {
-                        console.log(row.id)
-                        console.log(row.call_receiving_status)
-                        console.log(data.callReceivingStatus)
+deleteCallCard().then(r => console.log("Removing Old CallCard"))
+setDriverCallReceivingStatus().then(r => console.log("Set Drivers Call-Receiving-Status"))
 
-                        const updates = {};
-                        updates['/callReceivingStatus'] = row.call_receiving_status;
-                        await update(driverRef, updates)
-                    }
-                })
-            }
+async function deleteCallCard() {
+    try {
+        const [callIdList] = await mysql.promise().query(`select id from k_call where create_date < '2022-09-27'`)
+        for (const callInfo of callIdList) {
+            let callCardRef = await ref(frdb, `callList/${callInfo.id}`)
+            onValue(callCardRef, async snapshot => {
+                const callCard = snapshot.val()
+                if(callCard != null
+                    && (callCard.status === 'CALL_USER_CANCEL'
+                        || callCard.status === 'CALL_STAFF_CANCEL'
+                        || callCard.status ==='CALL_CANCELED'
+                        || callCard.status === 'CALL_WAITING')) {
+                    console.log('---')
+                    console.log(`${snapshot.key} / ${callCard.status} is removed from frdb`)
+                    // await remove(ref(frdb, `callList/${snapshot.key}`))
+                }
+            })
         }
-    })
-} catch (e) {
-    console.log({e})
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+async function setDriverCallReceivingStatus() {
+    try {
+        const [rows] = mysql.promise().query('select * from driver where is_deleted=false')
+        console.log(`result count : ${rows.length}`)
+        for (const row of rows) {
+            let driverRef = ref(frdb, '/driverList/' + row.id)
+            onValue(driverRef, async (snapshot) => {
+                const firebaseDriver = snapshot.val()
+                if (firebaseDriver != null && firebaseDriver.callReceivingStatus != null && (row.call_receiving_status != firebaseDriver.callReceivingStatus)) {
+                    console.log(`[${row.id}] RDB : ${row.call_receiving_status} --- FRDB : ${firebaseDriver.callReceivingStatus}`)
+                    const updates = {};
+                    updates['/callReceivingStatus'] = row.call_receiving_status;
+                    await update(driverRef, updates)
+                }
+            })
+
+        }
+    } catch (e) {
+        console.error(e)
+    }
 }
